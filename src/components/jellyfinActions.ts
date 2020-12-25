f832cbca29490a692677fdf6500833911838f41d
hawken <hawken@thehawken.org>
2020-12-25 23:11:26 +0100

    fix playerManager.stop -> onStopped to avoid telling the cast to stop when it shouldn't

--- src/components/jellyfinActions.ts
+++ src/components/jellyfinActions.ts
@@ -249,15 +249,6 @@ export function play($scope: GlobalScope): void {
     }
 }
 
-/**
- * Don't actually stop, just show the idle view after 20ms
- */
-export function stop(): void {
-    setTimeout(function () {
-        DocumentManager.setAppStatus('waiting');
-    }, 20);
-}
-
 export function getPlaybackInfo(
     item: BaseItemDto,
     maxBitrate: number,
